4033a407144bc6903ccd9e757414500be8a416ac
Yunes B <yunes.bakhush@gmail.com>
2021-03-28 16:51:42 +0600

    fix: validator fix V1.2

--- middlewares/validator.js
+++ middlewares/validator.js
@@ -32,8 +32,8 @@ const validateUserId = celebrate({
 
 const validateMovieCreation = celebrate({
   body: Joi.object().keys({
-    country: Joi.string().required().min(2).max(30),
-    director: Joi.string().required().min(2).max(30),
+    country: Joi.string().required().min(2).max(200),
+    director: Joi.string().required().min(2).max(200),
     duration: Joi.number().required(),
     year: Joi.string().required().min(2).max(5),
     description: Joi.string().required().min(2).max(5000),
@@ -62,8 +62,8 @@ const validateMovieCreation = celebrate({
         return value;
       }),
     movieId: Joi.number().required(),
-    nameRU: Joi.string().required().min(1).max(100),
-    nameEN: Joi.string().required().min(1).max(100),
+    nameRU: Joi.string().required().min(1).max(200),
+    nameEN: Joi.string().required().min(1).max(200),
   }),
 });
 
